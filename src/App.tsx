ce78247e29babfc84f0fbcbcb085cce089065478
Guilherme Lucena <guilherme.lucena17@gmail.com>
2024-08-01 16:12:51 -0300

    feat(Form.tsx): Validações ao formulário adicionadas

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
+import { useState } from 'react';
 import './App.css';
 import Form from './components/Form';
 
 function App() {
+  const [formVisible, setFormVisible] = useState(false);
+
+  const handleClick = () => {
+    setFormVisible((visible) => !visible);
+  };
+
   return (
     <>
       <h1>Gerenciador de senhas</h1>
-      <Form />
+      {
+        formVisible
+          ? <Form cancelProp={ handleClick } />
+          : (
+            <button onClick={ handleClick }>
+              Cadastrar nova senha
+            </button>
+          )
+      }
     </>
   );
 }
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,32 +1,115 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
-export default function Form() {
+type FormProps = {
+  cancelProp: () => void
+};
+
+export default function Form({ cancelProp } : FormProps) {
+  const [formValid, setFormValid] = useState(false);
+
+  const [formInfo, setFormInfo] = useState(
+    {
+      serviceName: '',
+      login: '',
+      password: '',
+      url: '',
+    },
+  );
+
+  const validatePassword = () => {
+    // verifica se a senha possui entre 8 a 16 caracteres
+    const passwordLength = formInfo.password.length >= 8
+    && formInfo.password.length <= 16;
+    // reg exp que verifica se a senha possui letras e números
+    const hasLettersAndNumbers = /^(?=.*[a-zA-Z])(?=.*\d)/.test(formInfo.password);
+    // reg exp que verifica se a senha possui um caractere especial
+    const hasSpecialCharacter = /[!@#$%^&*(),.?":{}|<>]/.test(formInfo.password);
+    // retorna true se as três condições acima são verdadeiras ou false se pelo menos uma não for verdadeira
+    return passwordLength && hasLettersAndNumbers && hasSpecialCharacter;
+  };
+
+  const validateForm = ():boolean => {
+    return formInfo.serviceName !== '' && formInfo.login !== '' && validatePassword();
+  };
+
+  useEffect(() => {
+    setFormValid(validateForm());
+  }, [formInfo]);
+
+  function resetForm() {
+    setFormInfo({
+      serviceName: '',
+      login: '',
+      password: '',
+      url: '',
+    });
+    setFormValid(false);
+  }
+
+  function handleChange(
+    event: React.ChangeEvent<HTMLInputElement>,
+  ) {
+    const { name, value } = event.target;
+    setFormInfo({
+      ...formInfo,
+      [name]: value,
+    });
+  }
+
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    resetForm();
+  }
+
   return (
-    <form>
+    <form onSubmit={ handleSubmit }>
       <label>
         Nome do Serviço
-        <input type="text" />
+        <input
+          name="serviceName"
+          value={ formInfo.serviceName }
+          required
+          type="text"
+          onChange={ handleChange }
+        />
       </label>
 
       <label>
         Login
-        <input type="text" />
+        <input
+          name="login"
+          value={ formInfo.login }
+          required
+          type="text"
+          onChange={ handleChange }
+        />
       </label>
 
       <label>
         Senha
-        <input type="password" />
+        <input
+          name="password"
+          value={ formInfo.password }
+          required
+          type="password"
+          onChange={ handleChange }
+        />
       </label>
 
       <label>
         URL
-        <input type="text" />
+        <input
+          name="url"
+          value={ formInfo.url }
+          type="text"
+          onChange={ handleChange }
+        />
       </label>
 
-      <button>
+      <button type="submit" disabled={ !formValid }>
         Cadastrar
       </button>
-      <button>
+      <button onClick={ cancelProp }>
         Cancelar
       </button>
     </form>
